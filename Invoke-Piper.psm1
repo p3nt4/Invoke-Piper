9ff07feed4b4be4c4f152e6b2a0797692388d541
p3nt4 <louis.nacfr@gmail.com>
2017-08-10 17:31:02 +1000

    Update Invoke-Piper.psm1

--- Invoke-Piper.psm1
+++ Invoke-Piper.psm1
@@ -12,13 +12,15 @@ Creates a local or remote port forwarding through named pipes.
 
 .EXAMPLE
 
-Invoke-PiperServer -bindPipe testPipe -destHost 127.0.0.1 -destPort 3389
-Invoke-PiperClient -destPipe testPipe -pipeHost 192.168.1.1 -bindPort 33389
+SERVER: Invoke-PiperServer -bindPipe testPipe -destHost 127.0.0.1 -destPort 3389
+SERVER: net use \\serverIP /USER:User
+CLIENT: Invoke-PiperClient -destPipe testPipe -pipeHost serverIP -bindPort 33389
 
 Creates a local port forwarding through pipe testPipe: -L 33389:127.0.0.1:3389
 
-Invoke-PiperServer -remote -bindPipe testPipe  -bindPort 33389
-Invoke-PiperClient -remote -destPipe testPipe -pipeHost 192.168.1.1 -destHost 127.0.0.1 -destPort 3389
+SERVER: Invoke-PiperServer -remote -bindPipe testPipe  -bindPort 33389
+SERVER: net use \\serverIP /USER:User
+CLIENT: Invoke-PiperClient -remote -destPipe testPipe -pipeHost serverIP -destHost serverIP -destPort 3389
 
 Creates a remote port forwarding through pipe testPipe: -R 33389:127.0.0.1:3389
 #>
